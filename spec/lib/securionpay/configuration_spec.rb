f10ae329a5a4a588816d651ebe435a8a1b488417
Grzegorz Wilczynski <gwilczynski@gmail.com>
2016-04-06 21:37:57 +0200

    add missing configuration specs

--- spec/lib/securionpay/configuration_spec.rb
+++ spec/lib/securionpay/configuration_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Securionpay::Configuration do
+  let(:secret_key) { double }
+
   subject do
-    described_class.new
+    described_class.new(secret_key)
   end
 
-  it 'to do' do
-    # TODO
-    expect(true).to be_truthy
+  describe '.secret_key' do
+    it 'gets secret key' do
+      expect(subject.secret_key).to be(secret_key)
+    end
+  end
+
+  describe '.service_url' do
+    it 'get service url' do
+      expect(subject.service_url).to eq('https://api.securionpay.com')
+    end
   end
 end
--- spec/lib/securionpay/processor_spec.rb
+++ spec/lib/securionpay/processor_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Securionpay::Processor do
+  let(:configuration) { Securionpay::Configuration.new(secret_key) }
+
   subject do
-    described_class.new
+    described_class.new(configuration)
   end
 
   it 'to do' do
